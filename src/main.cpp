f4614829b165d948a74a9fc37e5e28f65cad0393
Erik Martino <erik.martino@gmail.com>
2018-04-07 14:37:15 +0200

    Working humitemp

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,63 @@
 #include <Arduino.h>
 #define CAYENNE_PRINT Serial
-#include "CayenneMQTTESP8266.h"
+#include <CayenneMQTTESP8266.h>
+#include <DHT.h>
+#include "credentials.h"
 
 #define VIRTUAL_CHANNEL 1
 
-const char *MQTT_USERNAME =  "f7698140-108d-11e8-9094-712b97c1b05a";
-const char *MQTT_PASSWORD =  "c93e09b2d794c9caf357be5d514594fdbf745718";
-const char *MQTT_CLIENT_ID = "6ee2a210-390b-11e8-9beb-4d400e603e7e";
+// const char *MQTT_USERNAME = "usernamea";
+// const char *MQTT_PASSWORD = "password";
+// const char *MQTT_CLIENT_ID = "clientid";
 
-const char *SSID = "JaneOgErik";
-const char *WIFI_PASSWORD = "giraf50055005";
+// const char *WIFI_SSID = "ssid";
+// const char *WIFI_PASSWORD = "psk";
 
+#define PIN_D2 4
+
+DHT dht;
 int lastMillis;
 
 void setup()
 {
     Serial.begin(9600);
 
-    Cayenne.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID, SSID, WIFI_PASSWORD);
+    Cayenne.begin(MQTT_USERNAME, MQTT_PASSWORD, MQTT_CLIENT_ID, WIFI_SSID, WIFI_PASSWORD);
     pinMode(LED_BUILTIN, OUTPUT);
+
     lastMillis = 0;
+
+    dht.setup(PIN_D2, DHT::DHT22);
+}
+
+void checkSensors()
+{
+    float temperature = dht.getTemperature();
+    float humidity = dht.getHumidity();
+    const char *status = dht.getStatusString();
+
+    Serial.printf("DHT22: status=%s temp=%f humi=%f ", status, temperature, humidity);
+    Serial.println();
+
+    if (!isnan(temperature))
+    {
+        Cayenne.celsiusWrite(1, temperature);
+    }
+
+    if (!isnan(humidity))
+    {
+        Cayenne.virtualWrite(2, humidity, TYPE_RELATIVE_HUMIDITY, UNIT_PERCENT);
+    }
 }
 
 void loop()
 {
     Cayenne.loop();
 
-    //Publish data every 10 seconds (10000 milliseconds). Change this value to publish at a different interval.
-	if (millis() - lastMillis > 10000) {
-		lastMillis = millis();
-		//Write data to Cayenne here. This example just sends the current uptime in milliseconds.
-		// Cayenne.virtualWrite(0, lastMillis);
-		//Some examples of other functions you can use to send data.
-		Cayenne.celsiusWrite(1, 22.0);
-        Cayenne.virtualWrite(2, 50, TYPE_RELATIVE_HUMIDITY, UNIT_PERCENT);
-	}
+    //Publish data every 10 seconds (10000 milliseconds). 
+    if (millis() - lastMillis > 10000)
+    {
+        lastMillis = millis();
+        checkSensors();
+    }
 }
-
